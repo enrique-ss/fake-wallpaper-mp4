452edfed2365efbb8423a7257c4126fc3b5942cd
Enrique Silveira <105016903+enrique-ss@users.noreply.github.com>
2025-11-26 17:00:08 -0300

    update .bat

--- icializacao.bat
+++ icializacao.bat
@@ -1,2 +1,4 @@
 @echo off
-start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --start-fullscreen "local do seu arquivo" --no-first-run
+start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --start-fullscreen "local do seu arquivo index" --no-first-run
+
+echo adicione o local do seu arquivo index e me jogue no Winr + R -> shell:startup
