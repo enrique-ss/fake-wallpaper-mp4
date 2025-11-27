e1ae52006ae26a325d263fa0f2009fe5d6a264b4
Enrique Silveira <105016903+enrique-ss@users.noreply.github.com>
2025-11-27 13:55:30 -0300

    arrumado .bat para abrir no perfil convidado

--- icializacao.bat
+++ icializacao.bat
@@ -1,4 +1,2 @@
 @echo off
-start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --start-fullscreen "local do seu arquivo index" --no-first-run
-
-echo adicione o local do seu arquivo index e me jogue no Winr + R -> shell:startup
+start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --guest --start-fullscreen "file:///C:/Users/LUIZHENRIQUESILVEIRA/Desktop/Pessoal/fake-wallpaper-mp4/index.html" --no-first-run
