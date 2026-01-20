653cf1efe6518b10e39e94c6688d0e1bf901cda9
Enrique Silveira <105016903+enrique-ss@users.noreply.github.com>
2026-01-20 13:48:38 -0300

    Update icializacao.bat

--- icializacao.bat
+++ icializacao.bat
@@ -2,4 +2,4 @@
 REM Use o argumento --user-data-dir para criar um perfil dedicado,
 REM isolando a configuração de tela cheia (fullscreen) do seu Edge normal.
 
-start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --user-data-dir="C:\EdgeWallpaperProfile" --guest --start-fullscreen "file:///C:/Users/LUIZHENRIQUESILVEIRA/Desktop/Pessoal/fake-wallpaper-mp4/index.html" --no-first-run
+start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --user-data-dir="C:\EdgeWallpaperProfile" --guest --start-fullscreen "C:\Users\LUIZHENRIQUESILVEIRA\Documents\GitHub\fake-wallpaper\index.html" --no-first-run
